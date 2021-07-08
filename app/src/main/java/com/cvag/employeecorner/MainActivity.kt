01fa3d3c3072d4d40d2e5badff251c6a9586b4c9
SIVA3161 <sivaguru3161@gmail.com>
2021-07-08 16:51:24 +0530

    Works fine with JSON data

--- app/src/main/java/com/cvag/employeecorner/MainActivity.kt
+++ app/src/main/java/com/cvag/employeecorner/MainActivity.kt
@@ -4,6 +4,8 @@ package com.cvag.employeecorner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.d
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,9 +18,18 @@ import java.lang.StringBuilder
 const val BASE_URL = "https://employeedataonly-default-rtdb.firebaseio.com/"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myAdapter: MyAdapter
+    lateinit var linearLayoutManager: LinearLayoutManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerview_employees.setHasFixedSize(false)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerview_employees.layoutManager = linearLayoutManager
 
         getMyData()
     }
@@ -35,19 +46,30 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<Data>?>, response: Response<List<Data>?>) {
                 val responseBody = response.body()!!
 
-                val myStringBuilder = StringBuilder()
 
-                for( myData in responseBody){
-                    myStringBuilder.append("${myData.employee_age}"+" Years Old" )
-                    myStringBuilder.append("\n")
-                }
+//                  <-------------To check the JSON fetch------------>
 
-                txt1.text = myStringBuilder
+//                val myStringBuilder = StringBuilder()
+//
+//                for( myData in responseBody){
+//                    myStringBuilder.append("${myData.employee_age}"+" Years Old" )
+//                    myStringBuilder.append("\n")
+//                }
+//
+//                //txt1.text = myStringBuilder
+//                  <^-------------To check the JSON fetch------------^>
+
+                myAdapter = MyAdapter(baseContext,responseBody)
+                myAdapter.notifyDataSetChanged()
+                recyclerview_employees.adapter = myAdapter
+
+
+
 
             }
 
             override fun onFailure(call: Call<List<Data>?>, t: Throwable) {
-                Log.d("Main Activity", "onFailure: "+t.message)
+                d("Main Activity", "onFailure: "+t.message)
             }
         })
     }
---
+++ app/src/main/java/com/cvag/employeecorner/MyAdapter.kt
@@ -0,0 +1,45 @@
+package com.cvag.employeecorner
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.custom_list_items.view.*
+
+
+class MyAdapter(val context: Context, val employeeList: List<Data>): RecyclerView.Adapter<MyAdapter.ViewHolder> (){
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+            var EmpName: TextView
+            var EmpAge: TextView
+            var EmpSalary: TextView
+           // var  EmpImage: ImageView
+
+            init {
+                EmpName = itemView.employee_name
+                EmpAge = itemView.employee_age
+                EmpSalary = itemView.employee_salary
+                //EmpImage = itemView.profile_image
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var itemView = LayoutInflater.from(context).inflate(R.layout.custom_list_items, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.EmpName.text = employeeList[position].employee_name.toString()
+        holder.EmpAge.text = employeeList[position].employee_age.toString()
+        holder.EmpSalary.text = employeeList[position].employee_salary.toString()
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return employeeList.size
+    }
+
+}
