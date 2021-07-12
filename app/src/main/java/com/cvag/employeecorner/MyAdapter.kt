446a3a0d7c05d92ca7c9db453c18232e900e5145
SIVA3161 <sivaguru3161@gmail.com>
2021-07-12 15:19:34 +0530

    Profile Image fuctionalities were added

--- app/src/main/java/com/cvag/employeecorner/MyAdapter.kt
+++ app/src/main/java/com/cvag/employeecorner/MyAdapter.kt
@@ -4,37 +4,36 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.custom_list_items.view.*
 
 
-class MyAdapter(val context: Context, val employeeList: List<Data>): RecyclerView.Adapter<MyAdapter.ViewHolder> (){
+class MyAdapter(private val context: Context, private val employeeList: List<Data>): RecyclerView.Adapter<MyAdapter.ViewHolder> (){
 
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
-            var EmpName: TextView
-            var EmpAge: TextView
-            var EmpSalary: TextView
-            //var  EmpImage: ImageView
+            var empName: TextView = itemView.employee_name
+        var empAge: TextView = itemView.employee_age
+        var empSalary: TextView = itemView.employee_salary
+        var empImage: ImageView = itemView.profile_image
 
-            init {
-                EmpName = itemView.employee_name
-                EmpAge = itemView.employee_age
-                EmpSalary = itemView.employee_salary
-                //EmpImage = itemView.profile_image
-            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var itemView = LayoutInflater.from(context).inflate(R.layout.custom_list_items, parent, false)
+        val itemView = LayoutInflater.from(context).inflate(R.layout.custom_list_items, parent, false)
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.EmpName.text = employeeList[position].employee_name.toString()
-        holder.EmpAge.text = employeeList[position].employee_age.toString()
-        holder.EmpSalary.text = employeeList[position].employee_salary.toString()
-        // need to implement for EmpImage View ---->
+        holder.empName.text = employeeList[position].employee_name
+        holder.empAge.text = employeeList[position].employee_age.toString()
+        holder.empSalary.text = employeeList[position].employee_salary.toString()
+        val url = employeeList[position].profile_image
+        Picasso.get()
+            .load(url)
+            .into(holder.empImage)
 
     }
 
